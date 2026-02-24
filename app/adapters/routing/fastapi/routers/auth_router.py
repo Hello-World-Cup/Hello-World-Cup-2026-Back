ba35cbe41eda3c4defe4811e0b811feabc310791
Frankly <franklybautista0511@gmail.com>
2026-02-24 11:16:08 -0400

    feat: route /me implemented

--- app/adapters/routing/fastapi/routers/auth_router.py
+++ app/adapters/routing/fastapi/routers/auth_router.py
@@ -1,4 +1,5 @@
 from typing import Any
+from app.adapters.database.postgres.repositories.user_repository import UserRepository
 from fastapi import APIRouter, Depends
 from app.ports.driving.handler_interface import HandlerInterface
 from app.domain.dtos.user_dto import (
@@ -14,7 +15,10 @@ from app.adapters.database.dependencies import (
     get_register_user_handler,
     get_login_user_handler,
     get_current_user_payload,
+    get_db
 )
+from app.domain.exceptions.base_exceptions import UnauthorizedException
+from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -44,3 +48,16 @@ def signout(
 ) -> Any:
     """Cierra la sesión. Solo funciona si el usuario tiene un token JWT válido (sesión activa)."""
     return SignOutResponseDTO()
+
+@router.get("/me", response_model=ResultSchema[UserResponseDTO])
+@format_response
+def me(payload:dict=Depends(get_current_user_payload),db:Session=Depends(get_db))->Any:
+    email= payload.get("email")
+    if not email:
+        raise UnauthorizedException("Token inválido: falta email")
+
+    user = UserRepository(db).get_by_email(email)
+    if not user: 
+        raise UnauthorizedException("Usuario no existe o sesión inválida")
+    
+    return UserResponseDTO.from_orm(user)
